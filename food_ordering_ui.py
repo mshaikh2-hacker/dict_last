1fa044b718b0d305047ee6daf359e50034709edb
Musthak Ahmad Shaik <mshaikh2@murraystate.edu>
2024-10-13 20:03:51 -0500

    all once

--- food_ordering_ui.py
+++ food_ordering_ui.py
@@ -2,40 +2,71 @@
 import data
 import functions
 def show_main_menu():
+  order_items = list()
   while True:
-    print("Solomon diner") #edit to show your name
-    print("__________")
-    print('N for a new order')
-    print('X for close orders and print the check')
-    print('Q for quit')
+    print("\nMusthak diner") #edit to show your name
+    print("__________\n")
+    print('Insert N for a new order')
+    print('Insert X to close orders and print the check')
+    print('Insert C to change the order')
+    print('Insert D to check if your desired order is available')
+    print('Insert M for manager mode')
+    print('Insert Q to quit')
     user_menu_choice = input('Your choice: ')
     if user_menu_choice in 'Qq':
       break
     elif user_menu_choice in 'Xx':
-      print('This option prints the list of items ordered, extended price, total, Taxes, and Grand total ')
+      total = 0
+      for i in range(len(order_items)):
+        print(order_items[i][0],'x',order_items[i][1],'------',int(order_items[i][2])*int(order_items[i][0]))
+        total += int(order_items[i][2])*int(order_items[i][0])
+      print('Total amount to pay :', total)
     elif user_menu_choice in 'Nn':
-      print('New order')
-      make_order(user_menu_choice.upper())  #calls a function for adding to the orders
+      while True:
+        quantity,item,price = make_order() #calls a function for adding to the orders
+        print(quantity,'x',item)
+        order_items.append([quantity,item,price])
+        print('\nDo you want to add more items?')
+        choice = input('Press "y" for YES and "n" for NO\n')
+        if choice in "Yy":
+          continue
+        elif choice in "Nn":
+          break
+        else:
+          print('invalid choice')
+    elif user_menu_choice in 'Cc':
+      if len(order_items) == 0:
+        print("You haven't ordered anything yet")
+      else:
+        close_order(user_menu_choice.upper())
+        order_items = []
+    elif user_menu_choice in 'Dd':
+      functions.customer_request()
+    elif user_menu_choice in 'Mm':
+      functions.manage()
+    else:
+      print('invalid choice')
 
-def make_order(menu_choice):
-  print('Functionality for menu choice ', menu_choice)
+      
+def make_order():
   user_selection = functions.get_item_number()
   item_code, quantity = user_selection.split()
-  print(functions.get_item_information(item_code))
+  if item_code not in data.drink_items and int(quantity)>data.menu_items[f'{item_code}'][2]:
+    print('Sorry! we only have',data.menu_items[f'{item_code}'][2],'items left in stock, Please enter the quantity as per the stock.')
+    quantity,product,price = make_order()
+    if item_code not in data.drink_items and int(quantity)>data.menu_items[f'{item_code}'][2]:
+      data.menu_items[item_code][2]-int(quantity)
+      return quantity,product,price
+  data.menu_items[item_code][2]-int(quantity)
+  return quantity,functions.get_item_information(f'{item_code}')[0],functions.get_item_information(f'{item_code}')[1]
 
-def close_order(menu_choice):
-  print('Functionality for menu choice ', menu_choice)
-
-
+def close_order():
+  print('Previous order was removed, select N for new order')
 
 if __name__ == '__main__':
-    #initialize the lists
     drinks = []
     appetizers = []
     salads = []
     entrees = []
     dessert= []
-    #print(functions.get_item_information('D1'))
     show_main_menu()
-
-
--- functions.py
+++ functions.py
@@ -6,11 +6,9 @@ import data
 def get_item_information(item_code):
   """ this  function that will return the item name and price for a given item code.
     For example, find_menu_item('D2') should return Lemonade, and integer 3 as the result """
-  print(item_code)
-  for item in data.menu_items:
-    item_number, item_name, item_price = item.split(' ')
-    if item_number == item_code:
-      return item_name.encode("ascii", "ignore").decode(), int(item_price)
+  if item_code in data.menu_items:
+      item = str(item_code)
+      return data.menu_items[item]
 
 def display_items():
   pass
@@ -19,10 +17,76 @@ def get_item_number():
   while True:
     print('Drinks', [d.replace('\u200b','') for d in data.menu_items if d[0] == 'D'])
     print('Appetizers', [d.replace('\u200b','') for d in data.menu_items if d[0] == 'A'])
-    #write code for displaying the other dishes also
+    print('Salads', [d.replace('\u200b','') for d in data.menu_items if d[0] == 'S'])
+    print('Entrees', [d.replace('\u200b','') for d in data.menu_items if d[0] == 'E'])
+    print('Deserts', [d.replace('\u200b','') for d in data.menu_items if d[0] == 'T'])
+
     order_item = input('Enter dish number and quantity: ')
-    if order_item.split()[0] in data.all_items:
+    if order_item.split()[0].upper() in data.all_items:
       return order_item
     else:
-      print('Invalid dish number.  Please try again')
+      print('Invalid dish number. Please try again')
 
+def manage():
+  while True:
+    print("\nHello Manager") #edit to show your name
+    print("__________\n")
+    print('Insert R to remove an item')
+    print('Insert A to add an item')
+    print('Insert D to change decription of an item')
+    print('Insert S to update stock number of an item')
+    print('Insert P to update price of an item')
+    print('Insert Q to quit')
+    user_menu_choice = input('Your choice: ')
+    if user_menu_choice in 'Rr':
+      remove_choice = input('Enter the dish number of item  you want to remove: ')
+      data.menu_items.pop(remove_choice)
+      print(data.menu_items)
+    elif user_menu_choice in 'Aa':
+      dish_number = input('Enter dish number: ')
+      desc = input('Enter dish description: ')
+      cost = input('Enter dish price: ')
+      stock = input('Enter dish stock number: ')
+      data.menu_items[str(dish_number)] = list()
+      data.menu_items[str(dish_number)].append(str(desc))
+      data.menu_items[str(dish_number)].append(int(cost))
+      data.menu_items[str(dish_number)].append(int(stock))
+      print(data.menu_items)
+    elif user_menu_choice in 'Dd':
+      number = input('Enter the number of the dish you want to change the description of: ')
+      desciption = input('Enter description: ')
+      data.menu_items[number][0] = str(desciption)
+      print(data.menu_items)
+    elif user_menu_choice in 'Ss':
+      number = input('Enter the number of the dish you want to change the stock of: ')
+      stock_num = input('Enter new stock details: ')
+      data.menu_items[number][2] = int(stock_num)
+      print(data.menu_items) 
+    elif user_menu_choice in 'Pp':
+      number = input('Enter the number of the dish you want to change the price of: ')
+      price = input('Enter new price: ')
+      data.menu_items[number][1] = int(price) 
+      print(data.menu_items)
+    elif user_menu_choice in 'Qq':
+      break
+    else:
+      print('Invalid choice')
+
+def customer_request():
+     desired_item = input('Enter your desired order: ')
+     quantity = input('Enter Quantity')
+     dish_number = 0
+     for i in data.menu_items:
+        dish_number += 1
+        #print(data.menu_items[i][0])
+        #print(str(desired_item).upper())
+        if data.menu_items[i][0] == str(desired_item).upper() and int(quantity)>data.menu_items[i][2]:
+          print('we have what you ordered, but we only have',data.menu_items[i][2],'items in the stock')
+          print('Please dplace order as per the stock')
+        elif data.menu_items[i][0] == str(desired_item).upper() and int(quantity)<=data.menu_items[i][2]:
+          print('Great! we have what you ordered.')
+          print("Dish number is",i)
+          dish_number = i
+          break
+        elif dish_number == len(data.menu_items):
+          print('Sorry! we do not have what you ordered')
